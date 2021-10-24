55f3629f86b2afd6c35e3737702199aa2f2c109f
dabaisuv <54099873+dabaisuv@users.noreply.github.com>
2021-10-24 10:48:36 +0800

    Update linux_backlight_xrandr.sh

--- linux_backlight_xrandr.sh
+++ linux_backlight_xrandr.sh
@@ -17,6 +17,20 @@ xrandrlight_min=0.6
 xrandrlight_max=1
 brightness_current=$(cat $backlight_dir/$current_file)
 brightness_max=$(cat $backlight_dir/$max_file)
+brightness_percent=$(awk 'BEGIN{printf "%.6f",('$brightness_current'/'$brightness_max')*100}')
+xrandrlight_point=$(awk 'BEGIN{printf "%.6f",(''('$xrandrlight_max'-'$xrandrlight_min')''/'100')}')
+
+in_use=`xrandr | awk -F ' ' '{print $2}'`
+in_use_=`echo $in_use | awk '{split($0,a," ");
+for(i in a)
+    if(a[i]=="connected") print i}'`
+device=`xrandr | awk -F ' ' '{print $1}'`
+array=(${device// / })
+device_=${array[$in_use_-1]}
+
+xrandrlight_real=$(awk 'BEGIN{printf "%.6f",('$xrandrlight_min'+'$xrandrlight_point'*'$brightness_percent')}')
+
+xrandr --output $device_ --brightness $xrandrlight_real
 
 while(true)
 do
@@ -24,15 +38,8 @@ do
     then
         brightness_current=$(cat $backlight_dir/$current_file)
         brightness_percent=$(awk 'BEGIN{printf "%.6f",('$brightness_current'/'$brightness_max')*100}')
-        xrandrlight_point=$(awk 'BEGIN{printf "%.6f",(''('$xrandrlight_max'-'$xrandrlight_min')''/'100')}')
         xrandrlight_real=$(awk 'BEGIN{printf "%.6f",('$xrandrlight_min'+'$xrandrlight_point'*'$brightness_percent')}')
-        in_use=`xrandr | awk -F ' ' '{print $2}'`
-        in_use_=`echo $in_use | awk '{split($0,a," ");
-        for(i in a)
-            if(a[i]=="connected") print i}'`
-        device=`xrandr | awk -F ' ' '{print $1}'`
-        array=(${device// / })
-        device_=${array[$in_use_-1]}
+
         xrandr --output $device_ --brightness $xrandrlight_real
     fi
 
